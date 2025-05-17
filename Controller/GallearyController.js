74ca11eb1e0f21b59757ee09af369784981e4390
Razvifaizan <168092923+Razvifaizan@users.noreply.github.com>
2025-05-16 20:50:06 -1100

    Update GallearyController.js

--- Controller/GallearyController.js
+++ Controller/GallearyController.js
@@ -1,4 +1,4 @@
-const GalleryModel = require('../models/GalleryModel');
+const MediaModel = require('../Models/GalleryModel.js');
 const path = require('path');
 
 exports.addMedia = async (req, res) => {
